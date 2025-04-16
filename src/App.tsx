5ecc31bc5a209ecd6dea69351dd97c946eb24fcc
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-04-16 14:07:16 +0530

    feature: Use dimensions of terminal in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import { Text, useInput, useApp, Box } from 'ink';
+import { useEffect, useState } from 'react';
 
 function App() {
     const { exit } = useApp();
+    // const rows = process.stdout.rows;
+    // const columns = process.stdout.columns;
+    const [terminalDimensions, setTerminalDimensions] = useState({
+        width: process.stdout.columns,
+        height: process.stdout.rows,
+    });
+
+    useEffect(() => {
+        const handleResize = () => {
+            setTerminalDimensions({
+                width: process.stdout.columns,
+                height: process.stdout.rows,
+            });
+        };
+
+        process.stdout.on('resize', handleResize);
+
+        return () => {
+            process.stdout.off('resize', handleResize);
+        };
+    }, []);
 
     useInput((input) => {
         if (input.toLowerCase() === 'q') {
@@ -10,9 +32,22 @@ function App() {
     });
 
     return (
-        <Box flexDirection="column">
+        <Box
+            width={terminalDimensions.width}
+            height={terminalDimensions.height}
+            borderColor="white"
+            borderStyle="single"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            gap={1}
+        >
             <Text color="green">Hello, World ! </Text>
             <Text color="green">Press "q" to exit.</Text>
+            <Text>
+                Dimensions : {terminalDimensions.width} x{' '}
+                {terminalDimensions.height}
+            </Text>
         </Box>
     );
 }
