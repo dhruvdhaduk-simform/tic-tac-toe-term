746c0ebd921eeca9ebe30f072f94114b4cc18253
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-04-16 19:08:24 +0530

    feature: Handle the dimensions for Board to make it responsive

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -5,6 +5,19 @@ import { useContext } from 'react';
 function Board() {
     const terminalDimensions = useContext(DimensionContext);
 
+    let boardWidth: number;
+    let boardHeight: number;
+
+    const aspectRatio = terminalDimensions.width / terminalDimensions.height;
+
+    if (aspectRatio > 2) {
+        boardWidth = terminalDimensions.width / 2 - 2;
+        boardHeight = boardWidth / 2;
+    } else {
+        boardHeight = terminalDimensions.height / 2 - 2;
+        boardWidth = boardHeight * 2;
+    }
+
     return (
         <Box
             borderColor="white"
@@ -13,10 +26,21 @@ function Board() {
             justifyContent="center"
             alignItems="center"
             gap={1}
-            flexGrow={1}
+            // flexGrow={1}
         >
             <Box>
-                <Text>Board</Text>
+                <Box
+                    borderColor="white"
+                    borderStyle="single"
+                    flexDirection="column"
+                    justifyContent="center"
+                    alignItems="center"
+                    gap={1}
+                    width={boardWidth}
+                    height={boardHeight}
+                >
+                    <Text>Board</Text>
+                </Box>
             </Box>
         </Box>
     );
