f5da3273d6582abd886bd16c7b194d8385aeecee
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-04-17 17:46:53 +0530

    refractor: Remove 'consola' package and use node api instead

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,51 @@
 import { render } from 'ink';
 import App from './App';
-import consola from 'consola';
+import { createInterface } from 'readline';
 
 async function prompPlayerNames(): Promise<[string, string]> {
-    const player1Name = await consola.prompt('Enter Player 1 name :', {
-        default: 'Player 1',
-        placeholder: 'Player 1',
-        type: 'text',
+    const r1 = createInterface({
+        input: process.stdin,
+        output: process.stdout,
     });
 
-    const player2Name = await consola.prompt('Enter Player 2 name :', {
-        default: 'Player 2',
-        placeholder: 'Player 2',
-        type: 'text',
-    });
+    const ask = (question: string) => {
+        return new Promise((resolve) => r1.question(question, resolve));
+    };
+    const player1NameResponse = await ask('Enter Player 1 name : ');
+    const player2NameResponse = await ask('Enter Player 2 name : ');
+    let player1Name: string;
+    let player2Name: string;
 
-    console.log([player1Name, player2Name]);
+    if (
+        typeof player1NameResponse === 'string' &&
+        player1NameResponse.trim() !== ''
+    ) {
+        player1Name = player1NameResponse;
+    } else {
+        player1Name = 'Player 1';
+    }
 
-    return [player1Name, player2Name];
+    r1.close();
+
+    if (
+        typeof player2NameResponse === 'string' &&
+        player2NameResponse.trim() !== ''
+    ) {
+        player2Name = player2NameResponse;
+    } else {
+        player2Name = 'Player 2';
+    }
+
+    return [player1Name.trim(), player2Name.trim()];
 }
 
 prompPlayerNames().then((playerNames) => {
+    if (playerNames[0] === playerNames[1]) {
+        console.error(
+            'Please provide different names from Player 1 & Player 2.'
+        );
+        return;
+    }
     console.clear();
     render(<App player1Name={playerNames[0]} player2Name={playerNames[1]} />);
 });
